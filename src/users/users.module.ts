3435b2a1765cfe82367c6685ba7f96ea658f43a7
Sukhpreet Singh <sukhpreetsingh42350@gmail.com>
2026-09-24 16:27:46 +0530

    add account profile and verified email change

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,19 +1,41 @@
-import { forwardRef, Module } from "@nestjs/common";
+import {
+  forwardRef,
+  Module,
+} from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 
 import { AuthModule } from "../auth/auth.module";
 import { RolesGuard } from "../common/guards/roles.guard";
-import { User, UserSchema } from "./schemas/user.schema";
+import { VerificationModule } from "../verification/verification.module";
+
+import {
+  User,
+  UserSchema,
+} from "./schemas/user.schema";
 import { UsersController } from "./users.controller";
 import { UsersService } from "./users.service";
 
 @Module({
   imports: [
     forwardRef(() => AuthModule),
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+
+    VerificationModule,
+
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema,
+      },
+    ]),
   ],
+
   controllers: [UsersController],
-  providers: [UsersService, RolesGuard],
+
+  providers: [
+    UsersService,
+    RolesGuard,
+  ],
+
   exports: [UsersService],
 })
 export class UsersModule {}
